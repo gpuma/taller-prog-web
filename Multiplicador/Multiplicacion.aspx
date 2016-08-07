7c7e3fe4e052a99cdadd9a255257edecef8e7cd9
Gustavo Puma <vash47@gmail.com>
2016-08-07 15:09:24 -0500

    Agregado proyecto Multiplicador

---
+++ Multiplicador/Multiplicacion.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Multiplicacion.aspx.cs" Inherits="Multiplicador.Multiplicacion" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Multiplicación</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Ingrese dos números para multiplicar:
+        <asp:TextBox ID="txtNro1" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txtNro2" runat="server"></asp:TextBox>
+        <asp:Button ID="btnCalcular" runat="server" Text="Calcular" OnClick="btnCalcular_OnClick" />
+    </div>
+    <div>
+        <%--nuestro label de resultado es vacío al comienzo--%>
+        <asp:Label ID="lblResultado" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Multiplicador/Multiplicacion.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Multiplicador
+{
+    public partial class Multiplicacion : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        protected void btnCalcular_OnClick(object sender, EventArgs e)
+        {
+            //obtenemos el texto de nuestras cajas de texto
+            //y lo convertimos a entero
+            int nro1 = Convert.ToInt32(txtNro1.Text);
+            int nro2 = Convert.ToInt32(txtNro2.Text);
+
+            //usamos nuestra función
+            int resultado = Multiplicar(nro1, nro2);
+
+            //mostramos el resultado en nuestro label
+            lblResultado.Text = "El resultado es: " + resultado;
+        }
+    }
+}
