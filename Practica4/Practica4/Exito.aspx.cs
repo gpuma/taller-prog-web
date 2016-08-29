fa3a852fc22070977f92b1aadc4889917905c0da
Gustavo Puma <vash47@gmail.com>
2016-08-28 20:10:01 -0500

    Agregada actividad adicional

--- Practica4/Practica4/Exito.aspx.cs
+++ Practica4/Practica4/Exito.aspx.cs
@@ -11,7 +11,13 @@ namespace Practica4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+			//TODO: Actividad 5
+			//Cuando se cargue la página muestre el sgte. mensaje
+			//Bienvenido de nuevo, Nombres + Apellidos
+			//Donde nombres y apellidos, corresponden al usuario creado
+			//TIP: se recomienda que cree un nuevo Label para esto
+			
+			//TODO: su código va aquí
         }
     }
 }
