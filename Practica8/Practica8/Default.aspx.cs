49d41526c143aaec757e2133ca23c68b3dcee7a7
Gustavo Puma <vash47@gmail.com>
2016-09-20 11:39:58 -0500

    Conexión exitosa

---
+++ Practica8/Practica8/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Practica8
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var con_str = @"Data Source=.\SQLEXPRESS;
+                            AttachDbFilename=|DataDirectory|\Escuela.mdf;
+                            Integrated Security=True;
+                            Connect Timeout=30;
+                            Initial Catalog=Escuela;
+                            User Instance=True";
+
+            using (var con = new SqlConnection(con_str))
+            {
+                SqlCommand cmd = new SqlCommand("select * from NotaEstudiante", con);
+                con.Open();
+                var reader = cmd.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        Response.Write(String.Format("{0}, {1} <br/>", reader[0], reader[1]));
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+        }
+    }
+}
