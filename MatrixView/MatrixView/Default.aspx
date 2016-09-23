655d1f1ce0f7ef2295a4aee4c5b7f8d86d1298a4
Gustavo Puma <vash47@gmail.com>
2016-09-22 20:26:51 -0500

    Primer intento generación grid

---
+++ MatrixView/MatrixView/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MatrixView.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:GridView ID="grdMatriz" runat="server" AutoGenerateColumns="false">
+    <Columns>
+        <asp:TemplateField HeaderText="hola">
+            <ItemTemplate>
+                <asp:TextBox Text='<%# Eval("hola") %>' runat="server"/>
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+    </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ MatrixView/MatrixView/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace MatrixView
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int n = 2;
+            var dt = new DataTable();
+            dt.Columns.Add("hola");
+            //dt.Columns.Add();
+            //dt.Columns.Add();
+            dt.Rows.Add(0);
+            dt.Rows.Add(0);
+            dt.Rows.Add(0);
+            //dt.Rows.Add(0, 0, 0);
+            //dt.Rows.Add(0, 0, 0);
+
+            //grdMatriz.edi
+            grdMatriz.DataSource = dt;
+            grdMatriz.DataBind();
+        }
+    }
+}
