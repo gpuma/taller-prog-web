4ded6ca220e10ac47ff0a2efd3bd47e25d7580da
Gustavo Puma <vash47@gmail.com>
2016-11-06 23:56:02 -0500

    p9-no sirve seguridad

---
+++ Practica9/Practica9/Login.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//necesario para autenticación
+using System.Web.Security;
+
+namespace Practica9
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //usamos nuestra clase Seguridad
+            Seguridad.PrepararUsuariosYRoles();
+        }
+
+        protected void ctlLogin_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            Response.Write(Membership.GetAllUsers().Count);
+            if (FormsAuthentication.Authenticate(ctlLogin.UserName, ctlLogin.Password))
+                Response.Write("nice");
+            else
+                Response.Write("NOPE!");
+        }
+    }
+}
---
+++ Practica9/Practica9/confidencial/TopSecret.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TopSecret.aspx.cs" Inherits="Practica9.confidencial.TopSecret" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <h1>
+    Solo el administrador puede ver esto.
+    </h1>
+    </form>
+</body>
+</html>
