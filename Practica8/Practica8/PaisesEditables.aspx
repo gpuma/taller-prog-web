a1ce03206c26f83d4fe1912e41f60768012e883a
Gustavo Puma <vash47@gmail.com>
2016-12-09 19:00:20 -0500

    Practica8 - agregado gridview editable

---
+++ Practica8/Practica8/PaisesEditables.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PaisesEditables.aspx.cs" Inherits="Practica8.PaisesEditables" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Países</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <%--DataKeyNames necesario para poder recuperar el id de la fila--%>
+        <asp:GridView ID="grdPaises" AutoGenerateColumns="False" runat="server"
+            OnRowUpdating="grdPaises_RowUpdating"
+            OnRowEditing="grdPaises_RowEditing"
+            OnRowCancelingEdit="grdPaises_RowCancelingEdit"
+            DataKeyNames="id_pais">
+            <Columns>
+                <asp:CommandField ShowEditButton="True" />
+                <%-- podemos especificar un campo como solo lectura --%>
+                <asp:BoundField HeaderText="id" DataField="id_pais" ReadOnly="true" Visible="false"/>
+                <asp:BoundField HeaderText="Nombre" DataField="nom_pais" ReadOnly="true"/>
+                <asp:BoundField HeaderText="PBI" DataField="pbi_pais" />
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Practica8/Practica8/PaisesEditables.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practica8
+{
+    public partial class PaisesEditables : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Bind();
+            }
+        }
+
+        protected void grdPaises_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            //necesario setear el índice de edición para que entre a modo edición
+            grdPaises.EditIndex = e.NewEditIndex;
+            Bind();
+        }
+
+        protected void grdPaises_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            var p = new Pais()
+            {
+                //con e.Keys obtenemos el valor del campo que indicamos como primario
+                //para la fila que está siendo editado
+                id_pais = Convert.ToInt32(e.Keys[0]),
+                //los campos de solo lectura deben accederse directamente
+                nom_pais = grdPaises.Rows[e.RowIndex].Cells[2].Text,
+                //solo los campos que han sido editados aparecen en la colección e.NewValues
+                pbi_pais = Convert.ToDecimal(e.NewValues["pbi_pais"])
+            };
+            Modelo.ActualizarPais(p);
+            
+            //termina el modo de edición
+            grdPaises.EditIndex = -1;
+            Bind();
+        }
+        protected void Bind()
+        {
+            grdPaises.DataSource = Modelo.ObtenerPaises();
+            grdPaises.DataBind();
+        }
+        protected void grdPaises_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            grdPaises.EditIndex = -1;
+            Bind();
+        }
+
+
+    }
+}
